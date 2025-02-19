10587fc7482ab520185ee4a2d7a471a72734c798
Airam Plasencia <airaplas@gmail.com>
2025-02-19 09:58:47 +0000

    Delete Button

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 server.post('/movies', (req, res) => {
-    
-    const newMovie = req.body;
+  const newMovie = req.body;
+  const db = router.db; 
+  const movies = db.get('movies').value();
+
+  newMovie.id = movies.length ? Math.max(...movies.map(m => m.id)) + 1 : 1;
+  db.get('movies').push(newMovie).write();
+
+  res.status(201).json(newMovie);
+});
+
+
+server.delete('/movies/:id', (req, res) => {
+  const { id } = req.params;  
+  const db = router.db; 
+
   
-    
-    const db = router.db; 
+  const movie = db.get('movies').find({ id: parseInt(id) }).value();
   
-    
-    const movies = db.get('movies').value();
+  if (!movie) {
+      return res.status(404).json({ message: 'Película no encontrada' });
+  }
+
   
-    
-    newMovie.id = movies.length ? Math.max(...movies.map(m => m.id)) + 1 : 1;
+  db.get('movies').remove({ id: parseInt(id) }).write();
   
-    
-    db.get('movies').push(newMovie).write();
-  
-    
-    res.status(201).json(newMovie);
-  });
-  
+  res.status(200).json({ message: 'Película eliminada correctamente' });
+});
